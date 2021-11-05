a4b0fd3c04168f93e2f5641010e3234eab7eec3c
Leonardo <leonardoluan2017@gmail.com>
2021-11-04 21:43:51 -0300

    autenticando rotas

---
+++ src/pages/New/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+export default function New() {
+  return (
+    <View>
+      <Text>New</Text>
+    </View>
+
+
+  );
+}
