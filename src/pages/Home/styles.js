f7c2121c9a236dbc6535c039fd5f91ff56ae9db8
Leonardo <leonardoluan2017@gmail.com>
2021-11-06 14:33:46 -0300

    finalizacao da home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,20 +1,19 @@
 import styled from 'styled-components/native';
 
-
 export const Background = styled.View`
-flex:1;
-background-color: #131313;
+ flex:1;
+ background-color: #131313;
 `;
 
 export const Container = styled.View`
-margin-left: 15px;
-margin-bottom: 25px;
+    margin-left: 15px;
+    margin-bottom: 25px;
 `;
 
 export const Nome = styled.Text`
-font-size: 19px;
-color: #FFF;
-font-style: italic;
+    font-size: 19px;
+    color: #FFF;
+    font-style: italic;
 `;
 
 export const Saldo = styled.Text`
@@ -28,12 +27,18 @@ export const Title = styled.Text`
 margin-left: 15px;
 color: #00b94a;
 margin-bottom: 10px;
+
+`;
+export const List = styled.FlatList.attrs({
+    marginHorizontal: 15
+})`
+    padding-top: 15px;
+    background-color: #FFF;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    margin-left: 8px;
+    margin-right: 8px;
+
 `;
 
 
-
-
-
-
-
- 
